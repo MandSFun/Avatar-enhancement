4b31f99f13f68575cb81411fa2247644c6978933
LurkingGoo <Lucaskohyz@gmail.com>
2023-07-11 19:56:25 +0800

    NPC addition

--- Avatar/Assets/Main Scene Folder/Scripts/Gun/Shooting.cs
+++ Avatar/Assets/Main Scene Folder/Scripts/Gun/Shooting.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 
 public class Shooting : MonoBehaviour
-{public ParticleSystem[] muzzleflash;
-public ParticleSystem impactEffect;
-public GameObject shootOrigin;
-    public void isFiring(){
-        foreach(var particle in muzzleflash){
+{
+    public ParticleSystem[] muzzleflash;
+    public ParticleSystem impactEffect;
+    public GameObject shootOrigin;
+    public void isFiring()
+    {
+        foreach (var particle in muzzleflash)
+        {
             particle.Emit(1);
         }
         Shoot();
     }
-private void Shoot()
-{
-    RaycastHit hit;
-    if (Physics.Raycast(shootOrigin.transform.position, shootOrigin.transform.forward, out hit))
+    private void Shoot()
     {
-        impactEffect.transform.position = hit.point;
-        impactEffect.transform.rotation = Quaternion.LookRotation(hit.normal);
-        impactEffect.Emit(1);
+        RaycastHit hit;
+        if (Physics.Raycast(shootOrigin.transform.position, shootOrigin.transform.forward, out hit))
+        {
+            impactEffect.transform.position = hit.point;
+            impactEffect.transform.rotation = Quaternion.LookRotation(hit.normal);
+            impactEffect.Emit(1);
+        }
     }
-}
 
 
 
