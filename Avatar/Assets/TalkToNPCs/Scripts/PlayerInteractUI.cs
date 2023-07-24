0281b3e14dac523e49bac899938908e2f81a3f1b
LurkingGoo <Lucaskohyz@gmail.com>
2023-07-24 20:00:31 +0800

    Addition of blood pool, bug fixes

--- Avatar/Assets/TalkToNPCs/Scripts/PlayerInteractUI.cs
+++ Avatar/Assets/TalkToNPCs/Scripts/PlayerInteractUI.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.Netcode;
-public class PlayerInteractUI : NetworkBehaviour {
-
+public class PlayerInteractUI : NetworkBehaviour
+{
+    [SerializeField ]private PlayerInteractUI playerInteractUI;
     [SerializeField] private GameObject containerGameObject;
     [SerializeField] private PlayerInteract playerInteract;
     [SerializeField] private TextMeshProUGUI interactTextMeshProUGUI;
- public override void OnNetworkSpawn(){
-playerInteract = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInteract>();
- }
-    private void Update() {
-        if (playerInteract.GetInteractableObject() != null) {
+    public override void OnNetworkSpawn()
+    {   playerInteractUI.enabled = true; 
+        playerInteract = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInteract>();
+        containerGameObject.SetActive(true);
+    }
+    private void Update()
+    {   
+
+        if (playerInteract.GetInteractableObject() != null)
+        {
             Show(playerInteract.GetInteractableObject());
-        } else {    
+        }
+        else
+        {
             Hide();
         }
     }
 
-    private void Show(IInteractable interactable) {
+    private void Show(IInteractable interactable)
+    {
         containerGameObject.SetActive(true);
-        interactTextMeshProUGUI.text = interactable.GetInteractText();
+         interactTextMeshProUGUI.text = interactable.GetInteractText();
     }
 
-    private void Hide() {
+    private void Hide()
+    {
         containerGameObject.SetActive(false);
     }
 
